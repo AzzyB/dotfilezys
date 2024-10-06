3db4776ae2ae355649e3e55ece54c7e4d0378603
AzzyB <pokekid183@gmail.com>
2024-10-06 18:16:59 -0400

    nvim theme update

--- configs/nvim/lua/plugins/lsp-config.lua
+++ configs/nvim/lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "ts_ls" } --Place lsp servers here for respective languages
       })
     end
   },
@@ -19,6 +19,9 @@ return
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+
+      --Keybinds for lsp actions  
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
